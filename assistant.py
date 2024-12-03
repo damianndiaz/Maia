e16140283553cc905049ba18b6cab648b8dc3d1a
damianndiaz <diazzdamian00@gmail.com>
2024-12-03 10:07:32 -0300

    Update assistant.py

--- assistant.py
+++ assistant.py
@@ -8,9 +8,7 @@ def get_assistant_answer(client, user_msg, thread_id=None, assistant_id="asst_5L
     if not thread_id:
         print("Generando nuevo thread...")
         thread = client.beta.threads.create(
-            messages=[
-                {"role": "assistant", "content"}
-            ]
+            messages=[]  
         )
         thread_id = thread.id
         print(f"Nuevo thread creado. ID: {thread_id}")
